bf954a503f6b63f9723c66982e72ad85dd784ee6
zcSkr <524843954@qq.com>
2018-11-20 17:57:48 +0800

    init

--- src/components/DescriptionList/Description.d.ts
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-export default class Description extends React.PureComponent<
-  {
-    term: React.ReactNode;
-    style?: React.CSSProperties;
-  },
-  any
-> {}
